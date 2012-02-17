ede52a7e519632a5a335504e7fedbbe45289682e
Christopher Parks <christopher.daniel.parks@gmail.com>
2012-02-17 14:04:19 -0500

    Basic Node class and ParseError

--- src/parser.py
+++ src/parser.py
@@ -3,7 +3,7 @@
 Grammar for Mirza
 
 Program:
-    Expressions
+    Expressions EOF
 
 Expressions:
     Expression . Expressions | EMPTY
@@ -24,3 +24,24 @@ Formals:
     IDENTIFIER Formals | EMPTY
 
 """
+
+from tokens import lexemes
+from lexer import lexer
+
+class Node(object):
+    def __init__(self, root):
+        self.root = root
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def __str__(self):
+        return "[%s %s]" % (self.root, self.child)
+
+class ParserError(Exception):
+    pass
+
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
